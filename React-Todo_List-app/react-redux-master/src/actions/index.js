64a0e39c52d9f512a9f73eab2fb420c68896a790
ashley burns <burns.ashley331@gmail.com>
2024-04-23 13:20:24 -0400

    Creat counter app, Uses Redux action, reducer, and store,

--- React-Todo_List-app/react-redux-master/src/actions/index.js
+++ React-Todo_List-app/react-redux-master/src/actions/index.js
@@ -1,1 +1,11 @@
 //actions
+
+// Action to increment the counter. Argument: amount to be increased
+const increment = (val) =>{
+    return{
+        type: 'INCREMENT',
+        inc: val
+    }
+}
+
+export default increment;
